5332437f5f1f63135294b004afe528418685c093
cupaxx <fiqri.fa42@gmail.com>
2021-06-30 19:32:59 +0700

    add register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import RegisterTalent from './page/RegisterTalent'
+import RegisterRecruiter from './page/RegisterRecruiter'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={RegisterTalent} />
+          <Route path="/recruiter" component={RegisterRecruiter} />
+
+        </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/FormRegisterTalent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class FormRegisterTalent extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div className="flex flex-col mt-24 mr-12">
+                <h2 className="text-3xl font-semibold text-gray-800">Halo, Pewpeople</h2>
+                <h2 className="text-gray-700 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et duirhoncus auctor.</h2>
+                <div className="flex flex-col mt-10">
+                <label className="text-sm text-gray-500">Nama</label>
+                <input className="text-sm py-3 px-3 border-gray-200 border-2 rounded text-gray-600" type="text" placeholder="Masukan Nama Lengkap"></input>
+                <label className="text-sm text-gray-500 mt-8">Email</label>
+                <input className="text-sm py-3 px-3 border-gray-200 border-2 rounded text-gray-600" type="email" placeholder="Masukan Email"></input>
+                <label className="text-sm text-gray-500 mt-8">No Handphone</label>
+                <input className="text-sm py-3 px-3 border-gray-200 border-2 rounded text-gray-600" type="text" placeholder="Masukan No Handphone"></input>
+                <label className="text-sm text-gray-500 mt-8">Kata Sandi</label>
+                <input className="text-sm py-3 px-3 border-gray-200 border-2 rounded text-gray-600" type="password" placeholder="Masukan Kata Sandi"></input>
+                <label className="text-sm text-gray-500 mt-8">Konfirmasi kata Sandi</label>
+                <input className="text-sm py-3 px-3 border-gray-200 border-2 rounded text-gray-600" type="password" placeholder="Masukan Konfirmasi kata Sandi"></input>
+
+                <button type="submit" className="w-full bg-yellow-500 text-white font-semibold mt-10 py-3 rounded hover:bg-yellow-400">Daftar</button>
+                <h2 className="text-center mt-6">Anda Sudah Punya Akun? <a href="" className="text-yellow-500 hover:text-yellow-600 ">Masuk Disini</a> </h2>
+              </div>
+              </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FormRegisterTalent;
