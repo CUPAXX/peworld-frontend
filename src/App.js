db6d32c4914d6f25600181e06e59b970a3affd2b
cupaxx <fiqri.fa42@gmail.com>
2021-06-30 19:39:08 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 
-=======
 import React, { Component } from 'react';
 import RegisterTalent from './page/RegisterTalent'
 import RegisterRecruiter from './page/RegisterRecruiter'
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
->>>>>>> page-register-fiqry
 
 function App () {
   return (
