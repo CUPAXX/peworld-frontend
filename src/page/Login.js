5f8baa2af23bdb8f3ea1933dd9edfe3b0ad6afd4
cupaxx <fiqri.fa42@gmail.com>
2021-07-02 14:20:54 +0700

    edit route

--- src/page/Login.js
+++ src/page/Login.js
@@ -3,6 +3,7 @@ import ImgMix from '../components/ImgMix'
 import Halo from '../components/Halo'
 import ButtonLogin from '../components/ButtonLogin'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 export default class Login extends Component {
   render () {
@@ -22,12 +23,12 @@ export default class Login extends Component {
               <label className="text-gray-500 text-sm pt-8" >Kata Sandi</label>
               <input className="border border-gray-400 py-3 px-3 rounded text-gray-600 text-sm" type="password" placeholder="Masukan kata sandi" />
 
-              <div className="pt-3 pb-6 text-right" >Lupa kata sandi?</div>
+              <Link to="/forgotPassword" className="pt-3 pb-6 text-right" >Lupa kata sandi?</Link>
 
               <ButtonLogin />
               </form>
 
-              <div className="pt-6 text-center" >Anda belum punya akun? <span className="text-yellow-500"> Daftar disini </span>
+              <div className="pt-6 text-center" >Anda belum punya akun? <Link to="/register" className="text-yellow-500"> Daftar disini </Link>
               </div>
 
           </div>
--- src/page/PickerRegister.js
+++ src/page/PickerRegister.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ImgMix from '../components/ImgMix'
+import { Link } from 'react-router-dom'
 
 export default class PickerRegister extends Component {
   render () {
@@ -14,11 +15,11 @@ export default class PickerRegister extends Component {
           <div className="text-3xl pt-12 font-semibold pb-4 text-center mt-10">Register As</div>
           <div className="font-normal pb-8 text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</div>
 
-          <button className="bg-yellow-500 text-white text-base font-bold w-full rounded py-3 mb-8 hover:bg-yellow-400">Register as Talent</button>
+          <Link to="/register/talent"><button className="bg-yellow-500 text-white text-base font-bold w-full rounded py-3 mb-8 hover:bg-yellow-400">Register as Talent</button></Link>
 
-          <button className="bg-yellow-500 text-white text-base font-bold w-full rounded py-3 hover:bg-yellow-400">Register as Recruiter</button>
+          <Link to="/register/recruiter"><button className="bg-yellow-500 text-white text-base font-bold w-full rounded py-3 hover:bg-yellow-400">Register as Recruiter</button></Link>
 
-          <div className="pt-6 text-center" >Anda sudah punya akun? <span className="text-yellow-500"> Login disini </span>
+          <div className="pt-6 text-center" >Anda sudah punya akun? <Link to="/login" className="text-yellow-500"> Login disini </Link>
           </div>
       </div>
 
